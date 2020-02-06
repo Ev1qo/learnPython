7f4cdc032a846e671cd89bc68df7dba9c83184de
Oleg Vashenok <vashenok.o.v@gmail.com>
2020-02-07 01:05:55 +0200

    game is ready

--- game/game.py
+++ game/game.py
@@ -1,8 +1,27 @@
 import random
 
 print('Это игра "Угадай число"')
-print('Я загадал число от 1 до 100, у тебя есть 10 порыток чтобы угадать его')
-user_number = input('Введи свое число->')
-print(user_number)
-pc_number = random.randint(1, 10)
-print(pc_number)
+while 4==2+2:
+    print('Я загадал число от 1 до 10, у тебя есть 3 порытки чтобы угадать его')
+    pc_number = random.randint(1, 10)
+    print("Число от пк", + pc_number)
+    counter = 1
+    while counter < 4:
+        print("Попитка №", + counter)
+        user_number = int (input("Введи свое число ->"))
+        if user_number > pc_number:
+            print("\n")
+            print("Твое число больше")
+        elif user_number < pc_number:
+            print("\n")
+            print("Твое число меньше")
+        else:
+            print("\n")
+            print("Ты угадал")
+            break
+        counter += 1
+        print("\n")
+    else:
+        print("Ты проиграл") 
+    print("\n")   
+    
