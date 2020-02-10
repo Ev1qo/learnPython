5b46972f77d02e856efd9f1e38134b5fe60adf5b
Oleg Vashenok <vashenok.o.v@gmail.com>
2020-02-10 23:52:09 +0200

    turtle add

---
+++ turtle/turtlePractice.py
@@ -0,0 +1,14 @@
+import turtle
+
+while True:
+    turtle.shape('turtle')
+    turtle.speed(1)
+    # turtle.forward(50)
+    # turtle.left(90)
+    # turtle.forward(50)
+    # turtle.left(90)
+    # turtle.forward(50)
+    # turtle.left(90)
+    # turtle.forward(50)
+    # turtle.left(90)
+    turtle.circle(45)
