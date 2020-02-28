38967c55f24b66e5aff0d2673db95ccde7f96dbb
OlegVashenok <o.vashenok@starladder.tv>
2020-02-28 17:34:23 +0200

    binarySearch

---
+++ Algorithms/binarySearch.py
@@ -0,0 +1,20 @@
+def binary_search1(list, item):
+    low = 0
+    high = len(list)-1
+    print("hight", high)
+    while low <= high:
+        mid = (low + high)
+        print("mid", mid)
+        guess = list[mid]
+        if guess == item:
+            return mid
+        if guess > item:
+            high = mid - 1
+        else:
+            low = mid + 1
+    return None 
+
+my_list = [1, 3, 5, 7, 9]
+
+print(binary_search1(my_list, 3))
+print(binary_search1(my_list, -1))
